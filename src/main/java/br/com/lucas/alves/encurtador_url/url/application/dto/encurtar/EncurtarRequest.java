0b798798fa85f540fc584f2e3ac7083c61ef7278
Lucas Alves <olalvezluc@gmail.com>
2026-08-11 20:27:53 -0300

    Add EncurtarRequest and EncurtarResponse classes with constructors and getters

--- src/main/java/br/com/lucas/alves/encurtador_url/url/application/dto/encurtar/EncurtarRequest.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/url/application/dto/encurtar/EncurtarRequest.java
@@ -1,5 +1,13 @@
 package br.com.lucas.alves.encurtador_url.url.application.dto.encurtar;
 
 public class EncurtarRequest {
-    
+    private final String url;
+
+    public EncurtarRequest(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }  
 }
--- src/main/java/br/com/lucas/alves/encurtador_url/url/application/dto/encurtar/EncurtarResponse.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/url/application/dto/encurtar/EncurtarResponse.java
@@ -1,5 +1,19 @@
 package br.com.lucas.alves.encurtador_url.url.application.dto.encurtar;
 
 public class EncurtarResponse {
-    
+    private final String shortCode;
+    private final String shortUrl;
+
+    public EncurtarResponse(String shortCode, String shortUrl) {
+        this.shortCode = shortCode;
+        this.shortUrl = shortUrl;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
 }
