8862f8a1dce7a32546db1b29cff1d4230c33ce14
Lucas Alves <olalvezluc@gmail.com>
2026-08-12 21:18:02 -0300

    Add IUrlSwagger interface and UrlController class for URL shortening functionality

--- src/main/java/br/com/lucas/alves/encurtador_url/url/controller/url/UrlController.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/url/controller/url/UrlController.java
@@ -1,4 +1,4 @@
-package br.com.lucas.alves.encurtador_url.url.controller;
+package br.com.lucas.alves.encurtador_url.url.controller.url;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
