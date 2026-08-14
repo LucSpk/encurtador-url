061cfa47edb57f1cc142c3ade2226f0818a0a863
Lucas Alves <olalvezluc@gmail.com>
2026-08-13 21:08:57 -0300

    Implement URL shortening logic in UrlController

--- src/main/java/br/com/lucas/alves/encurtador_url/url/controller/url/UrlController.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/url/controller/url/UrlController.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lucas.alves.encurtador_url.url.application.dto.encurtar.EncurtarRequest;
 import br.com.lucas.alves.encurtador_url.url.application.dto.encurtar.EncurtarResponse;
+import br.com.lucas.alves.encurtador_url.url.application.service.UrlService;
 
 @RestController
 public class UrlController implements IUrlSwagger {
+
+    private final UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
     public ResponseEntity<EncurtarResponse> encurtarUrl(@RequestBody EncurtarRequest request) {
-        // TODO: Implementar a lógica para encurtar a URL recebida no request
-        return ResponseEntity.ok(null);
+        EncurtarResponse response = urlService.encurtarUrl(request);
+        return ResponseEntity.ok(response);
     }
 }
