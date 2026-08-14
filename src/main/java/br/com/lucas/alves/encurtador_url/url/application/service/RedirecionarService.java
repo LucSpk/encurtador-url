cca55c638453e0d25967cff9732c3877b26d8d0a
Lucas Alves <olalvezluc@gmail.com>
2026-08-13 21:09:04 -0300

    Add RedirecionarService and implement URL redirection logic in RedirecionarController

---
+++ src/main/java/br/com/lucas/alves/encurtador_url/url/application/service/RedirecionarService.java
@@ -0,0 +1,7 @@
+package br.com.lucas.alves.encurtador_url.url.application.service;
+
+public class RedirecionarService {
+    public String redirecionar(String shortCode) {
+        return "";
+    }
+}
--- src/main/java/br/com/lucas/alves/encurtador_url/url/controller/redirecionar/RedirecionarController.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/url/controller/redirecionar/RedirecionarController.java
@@ -3,9 +3,18 @@ package br.com.lucas.alves.encurtador_url.url.controller.redirecionar;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import br.com.lucas.alves.encurtador_url.url.application.service.RedirecionarService;
+
 public class RedirecionarController implements IRedirecionarSwagger {
+
+    private final RedirecionarService redirecionarService;
+
+    public RedirecionarController(RedirecionarService redirecionarService) {
+        this.redirecionarService = redirecionarService;
+    }
+
     public ResponseEntity<Void> redirect(String shortCode) {
-        // Implementar a lógica para redirecionar para a URL original com base no shortCode recebido
-        return ResponseEntity.status(HttpStatus.FOUND.value()).build();
+        String originalUrl = redirecionarService.redirecionar(shortCode);
+        return ResponseEntity.status(HttpStatus.FOUND.value()).header("Location", originalUrl).build();
     }
 }
