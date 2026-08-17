af63c87bf6a9bfa1ce9ccd0ba263412f717ba716
Lucas Alves <olalvezluc@gmail.com>
2026-08-17 00:01:18 -0300

    Add @RestController annotation to RedirecionarController

--- src/main/java/br/com/lucas/alves/encurtador_url/controller/redirecionar/RedirecionarController.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/controller/redirecionar/RedirecionarController.java
@@ -2,9 +2,11 @@ package br.com.lucas.alves.encurtador_url.controller.redirecionar;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lucas.alves.encurtador_url.application.service.IRedirecionarService;
 
+@RestController
 public class RedirecionarController implements IRedirecionarSwagger {
 
     private final IRedirecionarService redirecionarService;
