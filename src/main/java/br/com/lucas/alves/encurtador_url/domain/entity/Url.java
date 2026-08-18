330915f4d007db27c46332e30019f3d87c90a892
Lucas Alves <olalvezluc@gmail.com>
2026-08-18 19:48:58 -0300

    Add Url entity class to represent shortened URLs

---
+++ src/main/java/br/com/lucas/alves/encurtador_url/domain/entity/Url.java
@@ -0,0 +1,40 @@
+package br.com.lucas.alves.encurtador_url.domain.entity;
+
+public class Url {
+    private long id;
+    private String shortCode;
+    private String originalUrl;
+    private String createdAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }    
+}
