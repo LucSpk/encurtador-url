bbc6f93615538113107f98d9bd218483c14be607
Lucas Alves <olalvezluc@gmail.com>
2026-08-19 20:46:03 -0300

    Enable caching in the application and implement logging in RedirecionarService

--- src/main/java/br/com/lucas/alves/encurtador_url/EncurtadorUrlApplication.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/EncurtadorUrlApplication.java
@@ -2,8 +2,10 @@ package br.com.lucas.alves.encurtador_url;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class EncurtadorUrlApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/lucas/alves/encurtador_url/application/service/impl/RedirecionarService.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/application/service/impl/RedirecionarService.java
@@ -1,5 +1,8 @@
 package br.com.lucas.alves.encurtador_url.application.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import br.com.lucas.alves.encurtador_url.application.service.IRedirecionarService;
@@ -8,13 +11,17 @@ import br.com.lucas.alves.encurtador_url.repository.IUrlRepository;
 @Service
 public class RedirecionarService implements IRedirecionarService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedirecionarService.class);
+
     private final IUrlRepository urlRepository;
     
     public RedirecionarService(IUrlRepository urlRepository) {
         this.urlRepository = urlRepository;
     }
-   
+    
+    @Cacheable(value = "urls", key = "#shortCode")
     public String redirecionar(String shortCode) {
+        LOGGER.info("Consultando URL para código curto no banco de dados: {}", shortCode);
         return urlRepository.getUrlByShortened(shortCode);
     }
 }
