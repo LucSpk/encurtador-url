0cddde1ecf19e5b0c9b9b209324280e1fc29240e
Lucas Alves <olalvezluc@gmail.com>
2026-08-26 20:47:51 -0300

    Refactor IUrlSwagger to enhance API response documentation with content schemas for error responses

--- src/main/java/br/com/lucas/alves/encurtador_url/controller/url/IUrlSwagger.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/controller/url/IUrlSwagger.java
@@ -5,18 +5,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.lucas.alves.encurtador_url.application.dto.encurtar.*;
+import br.com.lucas.alves.encurtador_url.handlers.dto.ApiExceptionResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @ApiResponses(value = {
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Requisição inválida"),
-    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-})
+    @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content(
+        schema = @Schema(implementation = ApiExceptionResponse.class))),
+    @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content(
+        schema = @Schema(implementation = ApiExceptionResponse.class)))
+    }
+)
 public interface IUrlSwagger {
     
-    
+    @Operation(summary = "Encurta uma URL", operationId = "encurtarUrl", description = "Recebe uma URL original e retorna uma URL encurtada correspondente.")
     @ApiResponses(value = {
-        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "URL encurtada com sucesso")
+        @ApiResponse(responseCode = "201", description = "URL encurtada com sucesso", content = @Content(
+            schema = @Schema(implementation = EncurtarResponse.class)))
     })
     @PostMapping("/url")
     public ResponseEntity<EncurtarResponse> encurtarUrl(@Valid @RequestBody EncurtarRequest request);
