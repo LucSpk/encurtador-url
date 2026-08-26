1f7dac2d4cafb8f26ccf3a6a75e1d42a47286f0b
Lucas Alves <olalvezluc@gmail.com>
2026-08-26 20:35:13 -0300

    Add springdoc-openapi dependency for OpenAPI documentation support

--- src/main/java/br/com/lucas/alves/encurtador_url/controller/url/IUrlSwagger.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/controller/url/IUrlSwagger.java
@@ -5,10 +5,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.lucas.alves.encurtador_url.application.dto.encurtar.*;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
+@ApiResponses(value = {
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Requisição inválida"),
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+})
 public interface IUrlSwagger {
     
+    
+    @ApiResponses(value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "URL encurtada com sucesso")
+    })
     @PostMapping("/url")
     public ResponseEntity<EncurtarResponse> encurtarUrl(@Valid @RequestBody EncurtarRequest request);
 }
