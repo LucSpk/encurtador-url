852d99dbb7daf25e9965e1197204dba495fec9ca
Lucas Alves <olalvezluc@gmail.com>
2026-08-25 22:27:58 -0300

    Add validation annotations to EncurtarRequest and update IUrlSwagger for request validation

--- src/main/java/br/com/lucas/alves/encurtador_url/application/dto/encurtar/EncurtarRequest.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/application/dto/encurtar/EncurtarRequest.java
@@ -1,6 +1,17 @@
 package br.com.lucas.alves.encurtador_url.application.dto.encurtar;
 
+import org.hibernate.validator.constraints.URL;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public class EncurtarRequest {
+    @NotBlank(message = "A URL não pode ser nula ou vazia.")
+    @URL
+    @Pattern(
+        regexp = "^https?://.*$",
+        message = "A URL deve usar http ou https"
+    )
     private final String url;
 
     public EncurtarRequest(String url) {
--- src/main/java/br/com/lucas/alves/encurtador_url/controller/url/IUrlSwagger.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/controller/url/IUrlSwagger.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.lucas.alves.encurtador_url.application.dto.encurtar.*;
+import jakarta.validation.Valid;
 
 public interface IUrlSwagger {
     
     @PostMapping("/url")
-    public ResponseEntity<EncurtarResponse> encurtarUrl(@RequestBody EncurtarRequest request);
+    public ResponseEntity<EncurtarResponse> encurtarUrl(@Valid @RequestBody EncurtarRequest request);
 }
