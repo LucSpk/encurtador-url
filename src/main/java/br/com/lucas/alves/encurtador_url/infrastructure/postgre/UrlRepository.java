0f2aa220a515c5e810b0aad4100234a7d9d2d63e
Lucas Alves <olalvezluc@gmail.com>
2026-09-04 20:49:01 -0300

    Fix package name typo in UrlRepository

--- src/main/java/br/com/lucas/alves/encurtador_url/infrastructure/postgre/UrlRepository.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/infrastructure/postgre/UrlRepository.java
@@ -1,4 +1,4 @@
-package br.com.lucas.alves.encurtador_url.infrastructure.postegre;
+package br.com.lucas.alves.encurtador_url.infrastructure.postgre;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
