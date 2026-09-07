02b8c70f62bd34fac28e329d95821277d8ef208c
Lucas Alves <olalvezluc@gmail.com>
2026-09-07 13:14:53 -0300

    Add display name to RedirecionarControllerTest for better test clarity

--- src/test/java/br/com/lucas/alves/encurtador_url/api/controller/redirecionar/RedirecionarControllerTest.java
+++ src/test/java/br/com/lucas/alves/encurtador_url/api/controller/redirecionar/RedirecionarControllerTest.java
@@ -8,10 +8,12 @@ import org.springframework.http.ResponseEntity;
 
 import br.com.lucas.alves.encurtador_url.application.ports.input.IRedirecionarInputPort;
 
+@DisplayName("Testes para RedirecionarController")
 class RedirecionarControllerTest {
     private RedirecionarController redirecionarController;
     
     @Test
+    @DisplayName("Quando uma URL curta válida é fornecida, então redireciona para a URL original com código de status 302")
     void whenValidShortUrlIsProvided_ThenRedirectToOriginalUrl_WithStatusCode302() {
         IRedirecionarInputPort redirecionarInputPort = Mockito.mock(IRedirecionarInputPort.class);
         Mockito.when(redirecionarInputPort.redirecionar(Mockito.anyString())).thenReturn("https://www.example.com");
