e3157520769271cb9cc81705ef89e354f2921107
Lucas Alves <olalvezluc@gmail.com>
2026-09-07 13:37:05 -0300

    Exemplo de utilização do mockito com annotations

--- src/test/java/br/com/lucas/alves/encurtador_url/api/controller/redirecionar/RedirecionarControllerTest.java
+++ src/test/java/br/com/lucas/alves/encurtador_url/api/controller/redirecionar/RedirecionarControllerTest.java
@@ -1,25 +1,35 @@
 package br.com.lucas.alves.encurtador_url.api.controller.redirecionar;
 
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.anyString;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
 import br.com.lucas.alves.encurtador_url.application.ports.input.IRedirecionarInputPort;
 
+@ExtendWith(MockitoExtension.class)
 @DisplayName("Testes para RedirecionarController")
 class RedirecionarControllerTest {
+    @Mock 
+    private IRedirecionarInputPort redirecionarInputPort;
+
+    @InjectMocks 
     private RedirecionarController redirecionarController;
     
     @Test
     @DisplayName("Quando uma URL curta válida é fornecida, então redireciona para a URL original com código de status 302")
     void whenValidShortUrlIsProvided_ThenRedirectToOriginalUrl_WithStatusCode302() {
-        IRedirecionarInputPort redirecionarInputPort = Mockito.mock(IRedirecionarInputPort.class);
-        Mockito.when(redirecionarInputPort.redirecionar(Mockito.anyString())).thenReturn("https://www.example.com");
+        when(redirecionarInputPort.redirecionar(anyString())).thenReturn("https://www.example.com");
     
-        redirecionarController = new RedirecionarController(redirecionarInputPort);
-
         ResponseEntity<Void> response = redirecionarController.redirect("shortCode");
         
         Assertions.assertEquals(302, response.getStatusCode().value());
