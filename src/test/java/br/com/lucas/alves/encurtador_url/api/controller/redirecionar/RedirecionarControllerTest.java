660946f2ea6c53f9b0b5c837e6709f47bcf541fc
Lucas Alves <olalvezluc@gmail.com>
2026-09-07 14:05:59 -0300

    Refactor RedirecionarControllerTest to use MockitoExtension and simplify assertions

--- src/test/java/br/com/lucas/alves/encurtador_url/api/controller/redirecionar/RedirecionarControllerTest.java
+++ src/test/java/br/com/lucas/alves/encurtador_url/api/controller/redirecionar/RedirecionarControllerTest.java
@@ -1,18 +1,20 @@
 package br.com.lucas.alves.encurtador_url.api.controller.redirecionar;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
 import br.com.lucas.alves.encurtador_url.application.ports.input.IRedirecionarInputPort;
 
+@ExtendWith(MockitoExtension.class)
 @DisplayName("Testes para RedirecionarController")
 class RedirecionarControllerTest {
     @Mock 
@@ -24,12 +26,11 @@ class RedirecionarControllerTest {
     @Test
     @DisplayName("Quando uma URL curta válida é fornecida, então redireciona para a URL original com código de status 302")
     void whenValidShortUrlIsProvided_ThenRedirectToOriginalUrl_WithStatusCode302() {
-        MockitoAnnotations.openMocks(this);
         when(redirecionarInputPort.redirecionar(anyString())).thenReturn("https://www.example.com");
     
         ResponseEntity<Void> response = redirecionarController.redirect("shortCode");
         
-        Assertions.assertEquals(302, response.getStatusCode().value());
-        Assertions.assertEquals("https://www.example.com", response.getHeaders().getLocation().toString());
+        assertEquals(302, response.getStatusCode().value());
+        assertEquals("https://www.example.com", response.getHeaders().getLocation().toString());
     }
 }
