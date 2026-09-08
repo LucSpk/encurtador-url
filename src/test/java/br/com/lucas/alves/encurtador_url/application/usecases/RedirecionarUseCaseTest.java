657aa78c3ab0137b4308ee966a26aa3c334ccec4
Lucas Alves <olalvezluc@gmail.com>
2026-09-08 20:12:07 -0300

    Add unit tests for RedirecionarUseCase to validate URL redirection functionality

---
+++ src/test/java/br/com/lucas/alves/encurtador_url/application/usecases/RedirecionarUseCaseTest.java
@@ -0,0 +1,39 @@
+package br.com.lucas.alves.encurtador_url.application.usecases;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.lucas.alves.encurtador_url.application.ports.output.IUrlOutputPort;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Testes para RedirecionarUseCase")
+class RedirecionarUseCaseTest {
+
+    @Mock
+    private IUrlOutputPort urlRepository;
+
+    @InjectMocks
+    private RedirecionarUseCase redirecionarUseCase;
+
+    @Test
+    @DisplayName("Quando um código curto válido é fornecido, então retorna a URL original")
+    void whenValidShortCodeIsProvided_ThenReturnOriginalUrl() {
+        String shortCode = "shortCode";
+        String originalUrl = "https://www.example.com";
+
+        when(urlRepository.getUrlByShortened(shortCode)).thenReturn(originalUrl);
+
+        String result = redirecionarUseCase.redirecionar(shortCode);
+
+        assertEquals(originalUrl, result);
+        verify(urlRepository).getUrlByShortened(shortCode);
+    }
+}
