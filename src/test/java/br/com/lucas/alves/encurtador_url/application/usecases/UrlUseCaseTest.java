4760b11048880c26daba72e489fe88cc4945069d
Lucas Alves <olalvezluc@gmail.com>
2026-09-09 20:43:55 -0300

    Add initial unit test for UrlUseCase to validate URL shortening functionality

---
+++ src/test/java/br/com/lucas/alves/encurtador_url/application/usecases/UrlUseCaseTest.java
@@ -0,0 +1,10 @@
+package br.com.lucas.alves.encurtador_url.application.usecases;
+
+import org.junit.jupiter.api.Test;
+
+public class UrlUseCaseTest {
+    @Test
+    void testEncurtarUrl() {
+
+    }
+}
