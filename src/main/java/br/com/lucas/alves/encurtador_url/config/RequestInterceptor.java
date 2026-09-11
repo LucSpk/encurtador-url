d1f6bbe1f75bb370e741d1e2d7f0726406aff8eb
Lucas Alves <olalvezluc@gmail.com>
2026-09-10 22:21:10 -0300

    Implement RequestInterceptor to manage trace IDs and enhance logging

--- src/main/java/br/com/lucas/alves/encurtador_url/config/RequestInterceptor.java
+++ src/main/java/br/com/lucas/alves/encurtador_url/config/RequestInterceptor.java
@@ -2,6 +2,8 @@ package br.com.lucas.alves.encurtador_url.config;
 
 import java.io.IOException;
 
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -9,7 +11,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class RequestInterceptor extends OncePerRequestFilter {
+
+    private static final String TRACE_ID = "traceId";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -19,8 +24,18 @@ public class RequestInterceptor extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+
+        String traceId = request.getHeader(TRACE_ID);
+        if(traceId == null || traceId.isBlank()) 
+            traceId = java.util.UUID.randomUUID().toString();
+        
+        MDC.put(TRACE_ID, traceId);
+        
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.remove(TRACE_ID);
+        }
     }
 
     private boolean isHealthEndPoint(HttpServletRequest request) {
