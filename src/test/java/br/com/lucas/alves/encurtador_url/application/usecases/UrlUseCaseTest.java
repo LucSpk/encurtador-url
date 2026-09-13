d6db244f42d6376c2146d97f845cc754dc785f75
Lucas Alves <olalvezluc@gmail.com>
2026-09-13 13:44:07 -0300

    Add test for handling duplicate URLs in UrlUseCase

--- src/test/java/br/com/lucas/alves/encurtador_url/application/usecases/UrlUseCaseTest.java
+++ src/test/java/br/com/lucas/alves/encurtador_url/application/usecases/UrlUseCaseTest.java
@@ -6,6 +6,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -13,10 +15,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DuplicateKeyException;
 
 import br.com.lucas.alves.encurtador_url.api.requests.EncurtarRequest;
 import br.com.lucas.alves.encurtador_url.api.responses.EncurtarResponse;
 import br.com.lucas.alves.encurtador_url.application.ports.output.IUrlOutputPort;
+import br.com.lucas.alves.encurtador_url.domain.entity.Url;
 
 @ExtendWith (MockitoExtension.class)
 @DisplayName("Testes para UrlUseCase")
@@ -50,6 +54,29 @@ class UrlUseCaseTest {
             assertEquals("http://localhost:8080/123456", response.getShortUrl());
 
             verify(urlRepository, times(1)).saveUrl("https://www.example.com", "123456");
+        }
+
+        @Test
+        @DisplayName ("Quando uma URL duplicada é fornecida, então retorna a resposta de encurtamento existente")
+        void whenDuplicateUrlIsProvided_ThenReturnsExistingShortenedUrl() {
+            when(urlRepository.saveUrl("https://www.example.com", "123456")).thenThrow(new DuplicateKeyException("Duplicate key"));
+
+            Url existingUrl = new Url();
+            existingUrl.setShortCode("123456");
+            existingUrl.setOriginalUrl("https://www.example.com");
+            existingUrl.setCreatedAt("2023-01-01T00:00:00Z");
+
+            when(urlRepository.getByUrl("https://www.example.com")).thenReturn(Optional.of(existingUrl));
+            
+            EncurtarRequest request = new EncurtarRequest("https://www.example.com");
+            EncurtarResponse response = urlUseCase.encurtarUrl(request, "123456");
+
+            assertNotNull(response);
+            assertEquals("123456", response.getShortCode());
+            assertEquals("http://localhost:8080/123456", response.getShortUrl());
+
+            verify(urlRepository, times(1)).saveUrl("https://www.example.com", "123456");
+            verify(urlRepository, times(1)).getByUrl("https://www.example.com");
         }
     }
 }
